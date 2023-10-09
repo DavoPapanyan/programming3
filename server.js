16214746a9b407c439ff0356ab70c303c3e2ca8e
Vahe2007 <vahe.gabrielyan1@tumo.org>
2023-10-09 19:35:39 +0400

    open in local host

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+const http = require('http')
+
+const server = http.createServer(app)
+
+app.use(express.static("."));
+
+app.get('/', (req, res)=>{
+    res.redirect('index.html')
+})
+server.listen(3500, ()=>{
+    console.log('Server is listening to port 3500');
+})
